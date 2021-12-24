c1beae2d43783e4a8915dce5926619b56432ec6e
BraianGazano <gazanobraian@gmail.com>
2021-12-24 19:16:55 -0300

    Agregadas las cartas de pokemones

--- src/components/Pokedex.jsx
+++ src/components/Pokedex.jsx
@@ -1,9 +1,43 @@
+import { useState, Component, useEffect } from "react";
 import Card from "./cards/Card";
+import { get } from "axios";
 
 const Pokedex = () => {
-    return ( 
-        <Card></Card>
-     );
-}
+
+    const [result, setResult] = useState([]);
+    const [poke, setPoke] = useState([]);
+    const [load, setLoad] = useState('true');
+    const arr = [];
+    
+    useEffect(() => {
+      fetch('https://pokeapi.co/api/v2/pokemon/?limit=50')
+      .then((response) => response.json())
+      .then((data) => setResult(
+      data.results.map((item) => {
+      fetch(item.url)
+      .then((response) => response.json())
+      .then((allpokemon) => arr.push(allpokemon));
+      setPoke(arr);}),));}, []);
+
+    setTimeout(() => {
+    setLoad(false);
+    }, 1000);
+    return (
+        <div className="App">
+            <div className='pokegallery'>
+            { load ? (
+            <p>Loading...</p>
+            ) : (
+            poke.map((img, i) => (
+            <div id={img.id} key={img.id}>
+                <Card sprites={img.sprites} types={img.types} name={img.name}></Card>
+            </div>
+            ))
+            )}
+            </div>
+        </div>
+    );
+    }
+
  
 export default Pokedex;
--- src/components/cards/Card.jsx
+++ src/components/cards/Card.jsx
@@ -1,8 +1,12 @@
-const Card = () => {
+const Card = ({types, name, sprites}) => {
     return ( 
-        <>
-            <h2>Soy un pokemon</h2>
-        </>
+        <div className='card' style={{ width: '10rem', height: '15rem', backgroundColor: '#F0F0C9' }}>
+            <img  src={sprites.front_default} alt='pokemon' />
+            <div>
+                <h5 >{name}</h5>
+                <h6>type: {types[0].type.name}</h6>
+            </div>
+        </div>
      );
 }
  
